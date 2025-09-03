3051490ca67f75446c14060ede51ea1f741b2ff9
Enzo Guimarães <127992419+EnzoGuimaraesBevilaqua@users.noreply.github.com>
2025-09-03 18:05:08 -0300

    Add files via upload

---
+++ Credenciais.py
@@ -0,0 +1,7 @@
+DB_PARAMS = {
+    "host": "localhost",
+    "database": "MotionKey",
+    "user": "postgres",
+    "password": "root", #A senha que você definiu para o usuário postgres
+    "port": "5432"
+}
