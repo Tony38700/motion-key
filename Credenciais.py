2cda65454b640299702893de01a475eb01697e5c
Antônio Lucas <129812080+Tony38700@users.noreply.github.com>
2025-09-04 11:18:10 -0300

    Delete Credenciais.py

--- Credenciais.py
+++
@@ -1,7 +0,0 @@
-DB_PARAMS = {
-    "host": "localhost",
-    "database": "MotionKey",
-    "user": "postgres",
-    "password": "root", #A senha que você definiu para o usuário postgres
-    "port": "5432"
-}
