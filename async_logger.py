8b86f94e5fbcdeab7afce6d4906f890475fb0b66
Gabriel Gonçalves <gabrielyuyu@hotmail.com>
2025-10-22 13:24:21 -0300

    otimização dos logs

---
+++ async_logger.py
@@ -0,0 +1,63 @@
+import threading
+import time
+from collections import deque
+
+
+class AsyncLogger:
+    def __init__(self, gesture_logger, debug_logger, batch_interval=2.0):
+        self.gesture_logger = gesture_logger
+        self.debug_logger = debug_logger
+        self.batch_interval = batch_interval
+
+        self.gesture_queue = deque()
+        self.debug_queue = deque()
+        self.last_batch_time = time.time()
+
+        self.running = True
+        self.thread = threading.Thread(target=self._process_queues, daemon=True)
+        self.thread.start()
+
+    def log_gesture(self, fingers, landmarks_list=None, hand_detector=None, confidence=None, bounding_box=None):
+        self.gesture_queue.append({
+            'fingers': fingers,
+            'landmarks_list': landmarks_list,
+            'hand_detector': hand_detector,
+            'confidence': confidence,
+            'bounding_box': bounding_box
+        })
+
+    def log_debug(self, method_name, *args):
+        self.debug_queue.append({
+            'method': method_name,
+            'args': args
+        })
+
+    def _process_queues(self):
+        while self.running:
+            current_time = time.time()
+
+            # Processa em lotes a cada intervalo
+            if current_time - self.last_batch_time >= self.batch_interval:
+                self._process_batch()
+                self.last_batch_time = current_time
+
+            time.sleep(0.1)  # Pequena pausa para não sobrecarregar
+
+    def _process_batch(self):
+        # Processa apenas o último gesto (evita repetição)
+        if self.gesture_queue:
+            last_gesture = self.gesture_queue[-1]
+            self.gesture_logger.log_gesture(**last_gesture)
+            self.gesture_queue.clear()
+
+        # Processa apenas os últimos 5 debug logs (limita quantidade)
+        if self.debug_queue:
+            recent_debugs = list(self.debug_queue)[-5:]  # Últimos 5
+            for debug in recent_debugs:
+                method = getattr(self.debug_logger, debug['method'])
+                method(*debug['args'])
+            self.debug_queue.clear()
+
+    def stop(self):
+        self.running = False
+        self.thread.join()
